0bca58a73a9bd993ed764bda0279bdb819bdb8ef
Johnathan Burgess <ryanburgess173@outlook.com>
2023-02-03 18:52:47 -0500

    another

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import env_config from './env_config.json';
+import React from "react";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import env_config from "./env_config.json";
+
+var ReactDOM = require("react-dom/client");
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-if(env_config.environment!=='production'){
+if (env_config.environment !== "production") {
   root.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
   );
-}else{
-  root.render(
-    <App />
-  );
+} else {
+  root.render(<App />);
 }
 
 // If you want to start measuring performance in your app, pass a function
